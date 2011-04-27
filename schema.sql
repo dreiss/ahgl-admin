27e9d9c7a9fc20771e269d680607fb4711291951
David Reiss <dreiss@fb.com>
2011-04-26 19:50:20 -0700

    Second push, with result entry and lineup display

--- schema.sql
+++ schema.sql
@@ -5,16 +5,43 @@ CREATE TABLE teams (
 
 CREATE TABLE maps (
   week INTEGER,
-  setnum INTEGER,
+  set_number INTEGER,
   mapname TEXT,
-  PRIMARY KEY (week, setnum)
+  PRIMARY KEY (week, set_number)
 );
 
 CREATE TABLE lineup (
   week INTEGER,
   team INTEGER,
-  setnum INTEGER,
+  set_number INTEGER,
   player TEXT,
   race TEXT,
-  PRIMARY KEY (week, team, setnum)
+  PRIMARY KEY (week, team, set_number)
+);
+
+CREATE TABLE matches (
+  week INTEGER,
+  match_number INTEGER,
+  home_team INTEGER,
+  away_team INTEGER,
+  PRIMARY KEY (week, match_number)
+);
+
+CREATE TABLE ace_matches (
+  week INTEGER,
+  match_number INTEGER,
+  home_player TEXT,
+  away_player TEXT,
+  PRIMARY KEY (week, match_number)
+);
+
+CREATE TABLE set_results (
+  week INTEGER,
+  match_number INTEGER,
+  set_number INTEGER,
+  home_winner INTEGER,
+  away_winner INTEGER,
+  forfeit INTEGER,
+  replay_hash TEXT,
+  PRIMARY KEY (week, match_number, set_number)
 );
--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import os
 import contextlib
 import logging
 import cgi
+import hashlib
+import collections
 
 def open_db(path):
   import sqlite3
@@ -16,13 +18,13 @@ def open_db(path):
 def app(environ, start_response):
   is_nfsn = "NFSN_SITE_ROOT" in environ
   if is_nfsn:
-    db_path = "/home/protected/ahgl-pre.sq3"
+    work_path = "/home/protected/ahgl_pre"
     request_uri = environ["REQUEST_URI"]
   else:
-    db_path = "./ahgl-pre.sq3"
+    work_path = "./data"
     request_uri = environ["PATH_INFO"]
 
-  week_number = 2
+  db_path = os.path.join(work_path, "ahgl-pre.sq3")
 
   if request_uri == "/debug":
     start_response("200 OK", [("Content-type", "text/plain")])
@@ -41,11 +43,101 @@ def app(environ, start_response):
           <body>
             <h1>AHGL Admin Page</h1>
             <ul>
+              <li><a href="/ahgl/show-lineup">Show Lineup</a>
               <li><a href="/ahgl/enter-lineup">Enter Lineup</a>
+              <li><a href="/ahgl/enter-result">Enter Result</a>
             </ul>
           </body>
         </html>
         """]
+
+  elif request_uri.endswith("/show-lineup"):
+    db = open_db(db_path)
+
+    start_response("200 OK", [("Content-type", "text/html")])
+
+    getdata = cgi.FieldStorage(
+        fp=environ['wsgi.input'],
+        environ=environ,
+    )
+
+    week = None
+    try:
+      week = int(getdata.getfirst("week"))
+    except (ValueError, TypeError):
+      pass
+
+    if not week:
+      with contextlib.closing(db.cursor()) as cursor:
+        cursor.execute("SELECT DISTINCT week FROM maps ORDER BY week")
+        weeks = [ int(row[0]) for row in cursor ]
+
+      return ["""
+        <html>
+          <head>
+            <title>AHGL Select Lineup Week</title>
+          </head>
+          <body>
+          """] + [
+              "<a href=\"?week=%d\">Week %d</a><br>" % (wk, wk)
+              for wk in weeks
+          ] + ["""
+          </body>
+        </html>
+      """]
+
+    with contextlib.closing(db.cursor()) as cursor:
+      cursor.execute("SELECT id, name FROM teams")
+      teams = dict(cursor)
+
+    with contextlib.closing(db.cursor()) as cursor:
+      cursor.execute("SELECT match_number, home_team, away_team FROM matches WHERE week = ?", (week,))
+      matches = dict((row[0], (row[1], row[2])) for row in cursor)
+
+    with contextlib.closing(db.cursor()) as cursor:
+      cursor.execute("SELECT set_number, mapname FROM maps WHERE week = ?", (week,))
+      maps = dict((row[0], row[1]) for row in cursor)
+
+    lineups = collections.defaultdict(dict)
+    with contextlib.closing(db.cursor()) as cursor:
+      cursor.execute("SELECT team, set_number, player, race FROM lineup WHERE week = ?", (week,))
+      for (team, set_number, player, race) in cursor:
+        lineups[team][set_number] = (player, race)
+
+    lineup_displays = []
+    for (match, (home, away)) in sorted(matches.items()):
+      lineup_displays.append("<h2>Match %d: %s vs %s</h2>"
+          % (match, cgi.escape(teams[home]), cgi.escape(teams[away])))
+      if lineups[home] and lineups[away]:
+        for setnum in range(1,5+1):
+          mapname = maps[setnum]
+          if setnum == 5:
+            homeplayer = ("ACE", "?")
+            awayplayer = ("ACE", "?")
+          else:
+            homeplayer = lineups[home][setnum]
+            awayplayer = lineups[away][setnum]
+          lineup_displays.append("%s (%s) < %s > (%s) %s<br>"
+              % tuple(cgi.escape(val) for val in (homeplayer[0], homeplayer[1], mapname, awayplayer[1], awayplayer[0])))
+      else:
+        displays = ["NOT ENTERED", "LINEUP ENTERED"]
+        lineup_displays.append(
+            displays[int(bool(lineups[home]))]
+            + " <> " +
+            displays[int(bool(lineups[away]))]
+            )
+
+    return [("""
+      <html>
+        <head>
+          <title>AHGL Lineup</title>
+        </head>
+        <body>
+          <h1>AHGL Lineup Week %d<h1>
+          %s
+        </body>
+      </html>
+    """ % (week, "".join(lineup_displays))).encode()]
 
   elif request_uri.endswith("/enter-lineup"):
     db = open_db(db_path)
@@ -54,10 +146,11 @@ def app(environ, start_response):
 
     values = {}
 
+    week_number = 3
     values["week_number"] = week_number
 
     with contextlib.closing(db.cursor()) as cursor:
-      cursor.execute("SELECT setnum, mapname FROM maps WHERE week = ?", (week_number,))
+      cursor.execute("SELECT set_number, mapname FROM maps WHERE week = ?", (week_number,))
       maps = dict((row[0], row[1]) for row in cursor)
 
     with contextlib.closing(db.cursor()) as cursor:
@@ -98,6 +191,8 @@ def app(environ, start_response):
 
   elif request_uri.endswith("/submit-lineup"):
     start_response("200 OK", [("Content-type", "text/html")])
+
+    week_number = 3
 
     db = open_db(db_path)
 
@@ -140,7 +235,7 @@ def app(environ, start_response):
 
       with contextlib.closing(db.cursor()) as cursor:
         cursor.execute(
-            "INSERT INTO lineup(week, team, setnum, player, race) "
+            "INSERT INTO lineup(week, team, set_number, player, race) "
             "VALUES (?,?,?,?,?) "
             , (week_number, team, setnum, player, race))
 
@@ -153,6 +248,174 @@ def app(environ, start_response):
           </head>
           <body>
             <p>Lineup entered successfully.</p>
+          </body>
+        </html>
+        """]
+
+  elif request_uri.endswith("/enter-result"):
+    db = open_db(db_path)
+
+    start_response("200 OK", [("Content-type", "text/html")])
+
+    week_number = 2
+    values = {}
+
+    values["week_number"] = week_number
+
+    with contextlib.closing(db.cursor()) as cursor:
+      cursor.execute("SELECT match_number, ht.name, at.name FROM matches, teams ht, teams at WHERE week = ? AND ht.id = home_team AND at.id = away_team", (week_number,))
+      matches = dict((row[0], (row[1], row[2])) for row in cursor)
+
+    with contextlib.closing(db.cursor()) as cursor:
+      cursor.execute("SELECT set_number, mapname FROM maps WHERE week = ?", (week_number,))
+      maps = dict((row[0], row[1]) for row in cursor)
+
+    values["match_options"] = "".join([
+      "<option value=\"%d\">%s (Home) vs %s (Away)</option>"
+      % (matchnum, cgi.escape(ht), cgi.escape(at))
+      for (matchnum, (ht, at)) in matches.items() ])
+
+    fields = []
+
+    for setnum in range(1,5+1):
+      fields.append(
+          "<li>Game %d winner: "
+          "<input type=\"radio\" name=\"winner_%d\" value=\"home\">home "
+          "<input type=\"radio\" name=\"winner_%d\" value=\"away\">away "
+          % (setnum, setnum, setnum)
+          )
+      if setnum > 3:
+        fields.append(
+            "<input type=\"radio\" name=\"winner_%d\" value=\"none\">not played "
+            % (setnum,))
+      fields.append(
+          "| <input type=\"checkbox\" name=\"forfeit_%d\">forfeit "
+          "| Replay: <input type=\"file\" name=\"replay_%d\">"
+          % (setnum, setnum))
+
+    values["result_entries"] = "".join(fields)
+
+    return [("""
+        <html>
+          <head>
+            <title>AHGL Result Entry</title>
+          </head>
+          <body>
+            <h1>AHGL Result Entry</h1>
+            <h2>Week %(week_number)d</h2>
+            <form enctype="multipart/form-data" method="POST" action="/ahgl/submit-result">
+              <select name="match">
+              %(match_options)s
+              </select>
+              <ul>
+                %(result_entries)s
+                <li>
+                  Home ace: <input type="text" name="home_ace"></input>
+                  Away ace: <input type="text" name="away_ace"></input>
+              </ul>
+              <input type="submit">
+            </form>
+          </body>
+        </html>
+        """ % values).encode()]
+
+  elif request_uri.endswith("/submit-result"):
+    start_response("200 OK", [("Content-type", "text/html")])
+
+    week_number = 2
+    postdata = cgi.FieldStorage(
+        fp=environ['wsgi.input'],
+        environ=environ,
+    )
+
+    match = postdata.getlist("match")
+    if len(match) != 1 or not match[0]:
+      return ["No value submitted for 'match'"]
+    match = match[0]
+    try:
+      match = int(match)
+    except ValueError:
+      return ["Invalid match"]
+
+    sum_home = 0
+    sum_away = 0
+
+    winners = {}
+
+    for setnum in range(1, 5+1):
+      winner = postdata.getlist("winner_%d" % setnum)
+      if sum_home >= 3 or sum_away >= 3:
+        if setnum == 5 and winner != ["none"]:
+          return ["You played the ace match after one team won?"]
+        continue
+      if winner == ["home"]:
+        winners[setnum] = (1, 0)
+        sum_home += 1
+      elif winner == ["away"]:
+        winners[setnum] = (0, 1)
+        sum_away += 1
+      else:
+        return ["Invalid winner for set %d" % setnum]
+
+    num_sets = sum_home + sum_away
+
+    if num_sets == 5:
+      home_ace = postdata.getfirst("home_ace")
+      away_ace = postdata.getfirst("away_ace")
+      if not home_ace:
+        return ["No home ace specified."]
+      if not away_ace:
+        return ["No away ace specified."]
+
+    db = open_db(db_path)
+
+    with contextlib.closing(db.cursor()) as cursor:
+      cursor.execute("SELECT COUNT(*) FROM matches WHERE week = ? AND match_number = ?", (week_number, match))
+      if list(cursor) != [(1,)]:
+        return ["Invalid match"]
+
+    with contextlib.closing(db.cursor()) as cursor:
+      cursor.execute("SELECT COUNT(*) FROM set_results WHERE week = ? AND match_number = ?", (week_number, match))
+      if list(cursor) != [(0,)]:
+        return ["Result already submitted"]
+
+    rephashes = {}
+
+    for setnum in range(1, num_sets+1):
+      winner = postdata.getfirst("winner_%d" % setnum)
+      rep = postdata.getfirst("replay_%d" % setnum)
+      if not rep:
+        continue
+      rephash = hashlib.sha1(rep).hexdigest()
+      repfile = os.path.join(work_path, rephash + ".SC2Replay")
+      if not os.path.exists(repfile):
+        with open(repfile, "wb") as handle:
+          handle.write(rep)
+      rephashes[setnum] = rephash
+
+    for setnum in range(1, num_sets+1):
+      forfeit = 1 if postdata.getfirst("forfeit_%d" % setnum) == "on" else 0
+      wins = winners[setnum]
+      db.cursor().execute(
+          "INSERT INTO set_results(week, match_number, set_number, home_winner, away_winner, forfeit, replay_hash) "
+          "VALUES (?,?,?,?,?,?,?) "
+          , (week_number, match, setnum, wins[0], wins[1], forfeit, rephashes.get(setnum)))
+
+    if num_sets == 5:
+      db.cursor().execute(
+          "INSERT INTO ace_matches(week, match_number, home_player, away_player) "
+          "VALUES (?,?,?,?) "
+          , (week_number, match, home_ace, away_ace))
+
+    db.commit()
+
+    return ["""
+        <html>
+          <head>
+            <title>Success</title>
+          </head>
+          <body>
+            <p>Result entered successfully.</p>
           </body>
         </html>
         """]
